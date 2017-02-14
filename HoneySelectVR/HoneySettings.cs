cc4919ef1404a7a7be217ed4ce7d727c53e4af1c
Eusth <chikakarayattekita@yahoo.co.jp>
2017-02-14 20:22:59 +0100

    Add comments.

--- HoneySelectVR/HoneySettings.cs
+++ HoneySelectVR/HoneySettings.cs
@@ -10,9 +10,11 @@ namespace HoneySelectVR
     [XmlRoot("Settings")]
     public class HoneySettings : VRSettings
     {
+        [XmlComment("Whether or not to copy image effects from the main camera.")]
         public bool ApplyShaders { get { return _ApplyShaders; } set { _ApplyShaders = value; } }
         private bool _ApplyShaders = true;
 
+        [XmlComment("Blacklist of effects that are to be disable (because they don't look good in VR).")]
         public string[] EffectBlacklist { get { return _EffectBlacklist; } set { _EffectBlacklist = value; } }
         private string[] _EffectBlacklist = { "BloomAndFlares", "DepthOfField" };
     }
